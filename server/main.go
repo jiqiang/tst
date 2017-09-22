4a147af13da4a7c36bd1a89fbd5b7d169dbb1a1e
Qiang (Glenn) Ji <qiang.ji@ge.com>
2017-09-22 16:52:21 +1000

    use hub

--- server/main.go
+++ server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
@@ -13,12 +12,78 @@ import (
 	"github.com/jiqiang/tst/server/message"
 )
 
+// Hub maintains the set of active clients and broadcasts messages to the clients.
+type Hub struct {
+	// Registered clients
+	clients map[*Client]bool
+
+	// Inbound messages from the clients.
+	broadcast chan []byte
+
+	// Register requests from the clients.
+	register chan *Client
+
+	// Unregister requests from clients.
+	unregister chan *Client
+}
+
+func newHub() *Hub {
+	return &Hub{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
+
+// Client is a middleman between the websocket connection and the hub.
+type Client struct {
+	hub *Hub
+
+	// The websocket connection.
+	conn *websocket.Conn
+
+	// Buffered channel of outbound messages.
+	send chan []byte
+}
+
 const (
-	// Time allowed to read the next pong message from the client.
-	pongWait = 10 * time.Second
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this peroid. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
 )
 
-//testtest
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
@@ -27,63 +92,77 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func receiver(ws *websocket.Conn, out chan string) {
-	defer ws.Close()
-	ws.SetReadLimit(1024)
-	ws.SetReadDeadline(time.Time{})
-	ws.SetPongHandler(func(string) error {
-		ws.SetReadDeadline(time.Now().Add(pongWait))
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error {
+		c.conn.SetReadDeadline(time.Now().Add(pongWait))
 		return nil
 	})
 	for {
-		_, msg, err := ws.ReadMessage()
-		data := fmt.Sprintf("MESSAGE: %s", string(msg))
-		out <- data
+		_, _, err := c.conn.ReadMessage()
 		if err != nil {
-			log.Fatal(err)
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+				log.Printf("error: %v", err)
+			}
 			break
 		}
 	}
 }
 
-func sender(ws *websocket.Conn, in chan string) {
-	defer ws.Close()
+func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
 	for {
 		select {
-		case message := <-in:
-			err := ws.WriteMessage(websocket.TextMessage, []byte(message))
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
 			if err != nil {
-				log.Fatal(err)
+				return
+			}
+			w.Write(message)
+			// Close write.
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
 				return
 			}
 		}
 	}
 }
 
-func timer(out chan string) {
-	for {
-		data := message.Timer{
-			Type: "TIMER",
-			Time: time.Now().Format(time.ANSIC),
-		}
-		dataByte, _ := json.Marshal(data)
-		out <- string(dataByte)
-		time.Sleep(1 * time.Second)
-	}
-}
-
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
+func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println("upgrade:", err)
+		log.Println(err)
 		return
 	}
-	defer ws.Close()
-	c := make(chan string)
-	go sender(ws, c)
-	go timer(c)
-	go updateAssets(c)
-	receiver(ws, c)
+
+	client := &Client{
+		hub:  hub,
+		conn: conn,
+		send: make(chan []byte, 256),
+	}
+	client.hub.register <- client
+
+	go client.writePump()
+	go client.readPump()
 }
 
 func getMockAssets() message.Assets {
@@ -103,18 +182,37 @@ func getMockAssets() message.Assets {
 	return data
 }
 
-func updateAssets(out chan string) {
-	for {
-		data := getMockAssets()
-		assetsStr, _ := json.Marshal(data)
-		out <- string(assetsStr)
-		time.Sleep(3 * time.Second)
-	}
-}
-
 func main() {
+	hub := newHub()
+	go hub.run()
+
+	// Write timer
+	go func() {
+		for {
+			data := message.Timer{
+				Type: "TIMER",
+				Time: time.Now().Format(time.ANSIC),
+			}
+			dataByte, _ := json.Marshal(data)
+			hub.broadcast <- dataByte
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	// Write assets.
+	go func() {
+		for {
+			data := getMockAssets()
+			assetsStr, _ := json.Marshal(data)
+			hub.broadcast <- assetsStr
+			time.Sleep(3 * time.Second)
+		}
+	}()
+
 	r := mux.NewRouter()
-	r.HandleFunc("/ws", serveWs)
+	r.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
 	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("ui/"))))
 
 	srv := &http.Server{
